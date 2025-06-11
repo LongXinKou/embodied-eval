74b7ead65779cece30e1435f9e8a6ea3f53b8c23
klx <116343792+LongXinKou@users.noreply.github.com>
2025-06-11 10:31:33 +0800

    task

--- embodied_eval/tasks/__init__.py
+++ embodied_eval/tasks/__init__.py
@@ -14,6 +14,118 @@ import yaml
 
 from embodied_eval.utils import load_yaml_config, load_json
 
+# ======================= Task Config =======================
+@dataclass
+class TaskConfig(dict):
+    # task naming/registry
+    task: str = None
+    # HF dataset options: which dataset to use, and what splits for what purpose
+    dataset_path: str = None
+    dataset_name: str = None
+    load_from_disk: bool = False
+    test_split: str = None
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __setitem__(self, item, value):
+        return setattr(self, item, value)
+
+    def to_dict(self):
+        cfg_dict = asdict(self)
+        # remove values that are `None`
+        for k, v in list(cfg_dict.items()):
+            if v is None:
+                cfg_dict.pop(k)
+            elif isinstance(v, Callable):
+                cfg_dict[k] = str(v)
+        return cfg_dict
+
+# ======================= Task Object =======================
+class Task(abc.ABC):
+    """
+    A task represents an entire benchmark including its dataset, problems,
+    answers, and evaluation methods.
+    """
+
+    def __init__(
+            self,
+            config: Optional[dict] = None,
+            model_name: Optional[str] = None,
+    ) -> None:
+        self._config = TaskConfig(**config)
+        if self.config is None:
+            raise ValueError("Must pass a config to Task")
+
+        self.dataset_path = getattr(self.config, 'dataset_path', None)
+        self.dataset_name = getattr(self.config, 'dataset_name', None)
+        self.load_from_disk = getattr(self.config, 'load_from_disk', False)
+
+        # TODO model_name / metric config
+
+        self.prepare_dataset()
+        self.task_docs = self.test_docs()
+        self.features = list(self.task_docs.features.keys())
+
+        # TODO MC
+
+    @property
+    def config(self):
+        """Returns the TaskConfig associated with this class."""
+        return self._config
+
+    def test_docs(self) -> Dataset:
+        if self.config.test_split is not None:
+            return self.dataset[self.config.test_split]
+        else:
+            assert False, f"Task dataset (path={self.dataset_path}, name={self.dataset_name}) must have test docs!"
+
+    def prepare_dataset(self):
+        if not self.load_from_disk:
+            self.dataset = load_dataset(
+                path=self.dataset_path,
+                name=self.dataset_name,
+                download_mode=DownloadMode.REUSE_DATASET_IF_EXISTS,
+            )
+        else:
+            if self.dataset_path.endswith(".json"):
+                self.dataset = load_json(self.dataset_path)
+            elif self.dataset_path.endswith(".yaml"):
+                self.dataset = []
+                with open (self.dataset_path, "r") as f:
+                    yaml_data = yaml.safe_load(f)
+                    datasets = yaml_data.get("datasets", [])
+                    # datasts:
+                    #    - json_path: xxx1.json
+                    #      sampling_strategy: first/end/random:1000
+                    for dataset in datasets:
+                        json_path = dataset.get("json_path")
+                        sampling_strategy = dataset.get("sampling_strategy")
+                        sampling_number = None
+
+                        cur_dataset = load_json(json_path)
+                        if ":" in sampling_strategy:
+                            sampling_strategy, sampling_number = sampling_strategy.split(":")
+                            if "%" in sampling_number:
+                                sampling_number = math.ceil(int(sampling_number.split("%")[0]) * len(cur_dataset) / 100)
+                            else:
+                                sampling_number = int(sampling_number)
+                        if sampling_strategy == "first" and sampling_number is not None:
+                            cur_dataset = cur_dataset[:sampling_number]
+                        elif sampling_strategy == "end" and sampling_number is not None:
+                            cur_dataset = cur_dataset[-sampling_number:]
+                        elif sampling_strategy == "random" and sampling_number is not None:
+                            random.shuffle(cur_dataset)
+                            cur_dataset = cur_dataset[:sampling_number]
+
+                        self.dataset.extend(cur_dataset)
+                self.dataset = Dataset.from_list(self.dataset)
+            else:
+                self.dataset = load_from_disk(dataset_path=self.dataset_path)
+
+
+# ======================= Task Manager =======================
+
 class TaskManager:
     def __init__(
             self,
@@ -81,113 +193,24 @@ class TaskManager:
         all_loaded_tasks = dict(collections.ChainMap(*map(self._load_individual_task, task_list)))
         return all_loaded_tasks
 
-# ======================= Task Config =======================
-@dataclass
-class TaskConfig(dict):
-    # task naming/registry
-    task: str = None
-    # HF dataset options: which dataset to use, and what splits for what purpose
-    dataset_path: str = None
-    dataset_name: str = None
-    load_from_disk: bool = False
-    test_split: str = None
-    full_docs: bool = False
+def get_task_dict(
+    task_name_list: Union[str, List[Union[str, Dict, Task]]],
+    task_manager: Optional[TaskManager] = None,
+):
+    if isinstance(task_name_list, str):
+        task_name_list = [task_name_list]
+    string_task_name_list = [task for task in task_name_list if isinstance(task, str)]
 
-    def __getitem__(self, item):
-        return getattr(self, item)
+    if len(string_task_name_list) > 0:
+        if task_manager is None:
+            task_manager = TaskManager()
 
-    def __setitem__(self, item, value):
-        return setattr(self, item, value)
+        task_name_from_string_dict = task_manager.load_tasks(
+            string_task_name_list,
+        )
 
-    def to_dict(self):
-        cfg_dict = asdict(self)
-        # remove values that are `None`
-        for k, v in list(cfg_dict.items()):
-            if v is None:
-                cfg_dict.pop(k)
-            elif isinstance(v, Callable):
-                cfg_dict[k] = str(v)
-        return cfg_dict
+    final_task_dict = {
+        **task_name_from_string_dict,
+    }
 
-# ======================= Task Object =======================
-class Task(abc.ABC):
-    """
-    A task represents an entire benchmark including its dataset, problems,
-    answers, and evaluation methods.
-    """
-
-    def __init__(
-            self,
-            config: Optional[dict] = None,
-            model_name: Optional[str] = None,
-    ) -> None:
-        self._config = TaskConfig(**config)
-        if self.config is None:
-            raise ValueError("Must pass a config to Task")
-
-        self.dataset_path = getattr(self.config, 'dataset_path', None)
-        self.dataset_name = getattr(self.config, 'dataset_name', None)
-        self.load_from_disk = getattr(self.config, 'load_from_disk', False)
-
-        # TODO model_name / metric config
-
-        self.prepare_dataset()
-
-        self.task_docs = self.test_docs()
-        self.features = list(self.task_docs.features.keys())
-
-        # TODO MC
-
-    @property
-    def config(self):
-        """Returns the TaskConfig associated with this class."""
-        return self._config
-
-    def test_docs(self) -> Dataset:
-        if self.config.test_split is not None:
-            return self.dataset[self.config.test_split]
-        else:
-            assert False, f"Task dataset (path={self.dataset_path}, name={self.dataset_name}) must have test docs!"
-
-    def prepare_dataset(self):
-        if not self.load_from_disk:
-            self.dataset = load_dataset(
-                path=self.dataset_path,
-                name=self.dataset_name,
-                download_mode=DownloadMode.REUSE_DATASET_IF_EXISTS,
-            )
-        else:
-            if self.dataset_path.endswith(".json"):
-                self.dataset = load_json(self.dataset_path)
-            elif self.dataset_path.endswith(".yaml"):
-                self.dataset = []
-                with open (self.dataset_path, "r") as f:
-                    yaml_data = yaml.safe_load(f)
-                    datasets = yaml_data.get("datasets", [])
-                    # datasts:
-                    #    - json_path: xxx1.json
-                    #      sampling_strategy: first/end/random:1000
-                    for dataset in datasets:
-                        json_path = dataset.get("json_path")
-                        sampling_strategy = dataset.get("sampling_strategy")
-                        sampling_number = None
-
-                        cur_dataset = load_json(json_path)
-                        if ":" in sampling_strategy:
-                            sampling_strategy, sampling_number = sampling_strategy.split(":")
-                            if "%" in sampling_number:
-                                sampling_number = math.ceil(int(sampling_number.split("%")[0]) * len(cur_dataset) / 100)
-                            else:
-                                sampling_number = int(sampling_number)
-                        if sampling_strategy == "first" and sampling_number is not None:
-                            cur_dataset = cur_dataset[:sampling_number]
-                        elif sampling_strategy == "end" and sampling_number is not None:
-                            cur_dataset = cur_dataset[-sampling_number:]
-                        elif sampling_strategy == "random" and sampling_number is not None:
-                            random.shuffle(cur_dataset)
-                            cur_dataset = cur_dataset[:sampling_number]
-
-                        self.dataset.extend(cur_dataset)
-                self.dataset = Dataset.from_list(self.dataset)
-            else:
-                self.dataset = load_from_disk(dataset_path=self.dataset_path)
+    return final_task_dict
