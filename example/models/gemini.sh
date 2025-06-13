95b2aa0288049ddc02aeeae86ce0ab6bb86e007d
klx <116343792+LongXinKou@users.noreply.github.com>
2025-06-13 14:45:39 +0800

    gemini

--- example/models/gemini.sh
+++ example/models/gemini.sh
@@ -3,7 +3,7 @@ export OPENAI_API_BASE="https://yunwu.ai/v1"
 
 python -m embodied_eval \
     --model openai_compatible \
-    --model_args model_version=gemini-2.0-flash, \
+    --model_args model_name_or_path=gemini-2.0-flash, \
     --tasks where2place \
     --batch_size 1 \
     --output_path /data/klx/embodied-eval/logs/logs_robobrain/
